1701e933c6cf51184575ed50647dad120c0cd078
Max Foxie <altfoxie@icloud.com>
2024-01-21 02:40:06 +0300

    Fix

--- bot/meme.go
+++ bot/meme.go
@@ -148,7 +148,7 @@ func (b *Bot) onMeme(ctx context.Context, msg *tg.Message, builder *message.Requ
 }
 
 func (b *Bot) memeSearch(text string, userID int64) (io.Reader, string, error) {
-	photo, err := vkapi.SearchPhoto(strings.Split(text, "\n")[0], b.getToggleValue("last", userID))
+	photo, err := vkapi.SearchPhoto(strings.Split(text, "\n")[0], !b.getToggleValue("last", userID))
 	if err != nil {
 		return nil, "", wrapError(err, errImageNotFound)
 	}
